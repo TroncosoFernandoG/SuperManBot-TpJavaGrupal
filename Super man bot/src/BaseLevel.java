0e4ee078d007ef847246a8f5dfbe30a30314c93e
TroncosoFernandoG <fernandogtroncoso@gmail.com>
2025-09-24 03:55:36 -0300

    Delete Super man bot directory

--- Super man bot/src/BaseLevel.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Rectangle;
-import java.util.List;
-
-public abstract class BaseLevel {
-    public abstract List<enemigo> getEnemigos();
-    public abstract List<Rectangle> getBloquesDePiel();
-    public abstract Rectangle getMeta();
-}
